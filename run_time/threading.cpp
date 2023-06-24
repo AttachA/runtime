d0e6d6d88bfec4dafdc63174401b8dbda480ab79
Danyil Melnytskyi <ddann700@gmail.com>
2023-06-24 16:23:42 +0200

    Put license notice

--- run_time/threading.cpp
+++ run_time/threading.cpp
@@ -1,3 +1,8 @@
+// Copyright Danyil Melnytskyi 2022-2023
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 #include "threading.hpp"
 #include <Windows.h>
 namespace run_time {
