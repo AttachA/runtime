30ed32ff3e545e74541defa1abb5f1d912e5e7af
Danyil Melnytskyi <ddann700@gmail.com>
2023-07-22 18:48:19 +0200

    created wrapper for exception handling

--- src/run_time/library/exceptions.cpp
+++ src/run_time/library/exceptions.cpp
@@ -5,84 +5,31 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include "exceptions.hpp"
-#include "../asm/CASM.hpp"
-#include "../cxxException.hpp"
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-
+#include "../asm/exception.hpp"
 namespace art{
-    std::string inner_exception_info(const std::exception_ptr& inner_exception) {
-        CXXExInfo ex;
-        getCxxExInfoFromException(ex, inner_exception);
-    #ifdef _WIN64
-        if(ex.ex_ptr == nullptr) {
-            switch (ex.native_id) {
-            case EXCEPTION_ACCESS_VIOLATION:
-                return "\nAccessViolation: NATIVE EXCEPTION";
-            case EXCEPTION_STACK_OVERFLOW:
-                return "\nStackOverflow: NATIVE EXCEPTION";
-            case EXCEPTION_INT_DIVIDE_BY_ZERO:
-            case EXCEPTION_FLT_DIVIDE_BY_ZERO:
-                return "\nDivideByZero: NATIVE EXCEPTION";
-            case EXCEPTION_INT_OVERFLOW:
-            case EXCEPTION_FLT_OVERFLOW:
-            case EXCEPTION_FLT_STACK_CHECK:
-            case EXCEPTION_FLT_UNDERFLOW:
-                return "\nNumericOverflow: NATIVE EXCEPTION";
-            case EXCEPTION_BREAKPOINT: 
-                return "\nBreakpoint: NATIVE EXCEPTION";
-            case EXCEPTION_ILLEGAL_INSTRUCTION:
-                return "\nIllegalInstruction: NATIVE EXCEPTION";
-            case EXCEPTION_PRIV_INSTRUCTION:
-                return "\nPrivilegedInstruction: NATIVE EXCEPTION";
-            default:
-                return "\nUnknownNativeException: NATIVE EXCEPTION";
-            }
-    #else
-    #endif
-        } else {
-            try{
-                std::rethrow_exception(inner_exception);
-            }catch(const AttachARuntimeException& e) {
-                return "\n" + e.full_info();
-            }catch(const std::exception& e) {
-                return "\nC++Exception: " + std::string(e.what());
-            }catch(...) {
-                return "\nC++Exception: UNKNOWN";
-            }
+    namespace exceptions{
+        ValueItem* get_current_exception_name(ValueItem*, uint32_t){
+            return exception::get_current_exception_name();
         }
-    }
-        
-
-    std::string AttachARuntimeException::full_info() const {
-        std::string result = name() + std::string(": ") + what();
-        if (inner_exception) 
-            result += inner_exception_info(inner_exception);
-        return result;
-    }
-
-    InternalException::InternalException(const std::string& msq) : AttachARuntimeException(msq) {
-        auto tmp_trace = FrameResult::JitCaptureStackChainTrace();
-        stack_trace = {tmp_trace.begin(), tmp_trace.end()};
-    }
-    InternalException::InternalException(const std::string& msq, std::exception_ptr inner_exception) : AttachARuntimeException(msq, inner_exception) {
-        auto tmp_trace = FrameResult::JitCaptureStackChainTrace();
-        stack_trace = {tmp_trace.begin(), tmp_trace.end()};
-    }
-    std::string InternalException::full_info()  const {
-        std::string result = AttachARuntimeException::full_info();
-        result += "\nStackTrace:";
-        for (auto& frame : stack_trace) {
-            auto rframe = FrameResult::JitResolveFrame(frame);
-            result += "\n\t" + rframe.fn_name + ": " + std::to_string(rframe.line);
+        ValueItem* get_current_exception_desctription(ValueItem*, uint32_t){
+            return exception::get_current_exception_desctription();
         }
-        return result;
-    }
-        
-    std::string RoutineHandleExceptions::full_info() const {
-        std::string res = name() + std::string(": caught two exceptions in routine: ");
-        res += inner_exception_info(get_iner_exception());
-        res += inner_exception_info(second_exception);
-        return res;
+        ValueItem* get_current_exception_full_description(ValueItem*, uint32_t){
+            return exception::get_current_exception_full_description();
+        }
+        ValueItem* has_current_exception_inner_exception(ValueItem*, uint32_t){
+            return exception::has_current_exception_inner_exception();
+        }
+        ValueItem* unpack_current_exception(ValueItem*, uint32_t){
+            exception::unpack_current_exception();
+            return nullptr;
+        }
+        ValueItem* current_exception_catched(ValueItem*, uint32_t){
+            exception::current_exception_catched();
+            return nullptr;
+        }
+        ValueItem* in_exception(ValueItem*, uint32_t){
+            return new ValueItem(exception::has_exception());
+        }
     }
 }
